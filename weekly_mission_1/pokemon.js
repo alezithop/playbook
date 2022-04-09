61a71a363567f1e76b1a0e09f0dff5f75e41532e
Alezithop <alezithop@hotmail.com>
2022-04-08 23:58:16 -0500

    Added link for reference

---
+++ weekly_mission_1/pokemon.js
@@ -0,0 +1,15 @@
+class Pokemon {
+    constructor (pokemonName) {
+        this.pokemonName = pokemonName
+    }
+
+    sayHello() {
+        console.log(`Mi pokemon ${this.pokemonName} te saluda!!!`)
+    }
+
+    sayMessage(message) {
+        console.log(`Mi pokemon ${this.pokemonName} dice ${message}`)
+    }
+}
+
+module.exports = Pokemon
--- weekly_mission_1/readme.md
+++ weekly_mission_1/readme.md
@@ -1,1 +1,2 @@
 # Weekly Mission 1
+https://github.com/LaunchX-InnovaccionVirtual/MissionNodeJS/blob/main/semanas/semana_1.md
